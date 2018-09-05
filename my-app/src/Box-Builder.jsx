c87f76fae918eec4e42c89779e3379b6e2b762b2
Harsha Peddula <harsha@Harshas-MacBook-Air.local>
2018-09-05 12:43:26 +0530

    Add Conditional Rendering

--- my-app/src/Box-Builder.jsx
+++ my-app/src/Box-Builder.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Box-Builder.css';
 export default class BoxBuilder extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            boxes:['Harsha'],
+        }
+    }
     render() {
+        const count = this.state.boxes.length;
         return(
         <div>
             <h1 className="text-center">Box Builder</h1>
@@ -15,6 +23,11 @@ export default class BoxBuilder extends React.Component {
                     <button className="btn btn-danger">Remove Box</button>
                 </div>
                 <div className="col-sm-3"></div>
+            </div>
+            <div className="msg-wrapper">
+                {count >0 && <p className="counter">{count}</p>}
+                {count == 0 && `No boxes`
+                } 
             </div>
         </div>
         );
